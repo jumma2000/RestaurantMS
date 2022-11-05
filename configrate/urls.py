e3e8a16039fc20442f7804db850aee1183d9e5f2
jummaAli <jumma2000@hotmail.com.com>
2022-11-05 10:55:20 +0200

    add static files

--- configrate/urls.py
+++ configrate/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .import views
+
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('',views.home,name="home"),
+    path('user/',views.get_use,name="user"),
 ]
--- configrate/views.py
+++ configrate/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request,'configrate/header.html')
+
+
+def get_user(request):
+    return render(request,'configrate/user.html')
